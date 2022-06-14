347c3bc09a39219521e8f25a6716369be7977f4a
zhangzhen1979 <zhen.zhang@163.com>
2022-06-14 20:49:30 +0800

    启动类中加入cacheDir、reportfile文件夹是否存在的判断，不存在则自动创建。

--- src/main/java/com/thinkdifferent/reportserver/ReportApplication.java
+++ src/main/java/com/thinkdifferent/reportserver/ReportApplication.java
@@ -5,14 +5,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 
+import java.io.File;
+
 @EnableDiscoveryClient
 @EnableOpenApi
 @SpringBootApplication
 public class ReportApplication {
 
     public static void main(String[] args) {
+        checkPathExist("cacheDir");
+        checkPathExist("reportfile");
         // Druid监控页面：访问http://127.0.0.1:8080/druid/index.html
         SpringApplication.run(ReportApplication.class, args);
+    }
+
+    private static void checkPathExist(String strPath){
+        File fileCacheDir = new File(System.getProperty("user.dir") + "/"+strPath+"/");
+        if(!fileCacheDir.exists() || !fileCacheDir.isDirectory()){
+            fileCacheDir.mkdirs();
+        }
+
     }
 
 }
